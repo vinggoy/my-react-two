e0d62bb0b730980716c7407c090b030ca801c4a3
vingkris <vinggoy@users.noreply.github.com>
2018-09-20 14:31:48 +0800

    Added Name

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ const IndexPage = () => (
   <Layout>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>Ving Kris Saliendra</p>
     <Link to="/page-2/">Link Ako</Link>
   </Layout>
 )
